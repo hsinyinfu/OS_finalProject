5b3aea90d14e6d18c716d8866003af9a7a860653
oblivion <oblivion8514@gmail.com>
2016-12-06 19:27:35 +0800

    add SemaphoreClass.h QQ

---
+++ SemaphoreClass.h
@@ -0,0 +1,30 @@
+#include <mutex>
+#include <condition_variable>
+using namespace std;
+
+class Semaphore
+{
+	public:
+		Semaphore(int count_ = 0) : count(count_){}
+		void signal()
+		{
+			unique_lock<mutex> lck(mtx);
+			++count;
+			cv.notify_one();
+		}
+		void wait()
+		{
+			unique_lock<mutex> lck(mtx);
+			while(count == 0)
+			{
+				cv.wait(lck);
+			}
+
+			--count;
+		}
+
+	private:
+		mutex mtx;
+		condition_variable cv;
+		int count;
+};
