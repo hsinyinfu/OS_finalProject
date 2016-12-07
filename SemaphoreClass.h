3415b7683c3fe33b201e57e513d11cc2525a3d64
oblivion <oblivion8514@gmail.com>
2016-12-07 19:01:47 +0800

    add Semaphore's comment

--- SemaphoreClass.h
+++ SemaphoreClass.h
@@ -1,30 +1,34 @@
 #include <mutex>
 #include <condition_variable>
-using namespace std;
 
 class Semaphore
 {
 	public:
 		Semaphore(int count_ = 0) : count(count_){}
+
+		//leaving critical section and wake up someone who is waiting
 		void signal()
 		{
-			unique_lock<mutex> lck(mtx);
+			std::unique_lock<std::mutex> lck(mtx);	//acquire lock
 			++count;
-			cv.notify_one();
+			cv.notify_one();	//It wakes up another one sleeping
 		}
+
+		//waiting for enter critical section
 		void wait()
 		{
-			unique_lock<mutex> lck(mtx);
-			while(count == 0)
-			{
-				cv.wait(lck);
-			}
+			std::unique_lock<std::mutex> lck(mtx);	//acquire lock
+			while(count == 0)	//if count==0,which means someone is in its
+			{					//critical section,so that others have to 
+				cv.wait(lck);	//wait.When they in this line,they release
+			}					//the lock.
 
-			--count;
-		}
+			--count;			//If someone is notified so it leave the
+		}						//while-loop,they reacquire the lock and
+								//count--.
 
 	private:
-		mutex mtx;
-		condition_variable cv;
+		std::mutex mtx;
+		std::condition_variable cv;
 		int count;
 };
