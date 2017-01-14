812417a382a0414d7cd514b14f4148fdb5c813ba
hsinyinfu <peter0930260535@gmail.com>
2017-01-14 17:52:05 +0800

    完成主程式(包括thread以及poisson random)，並完成註解

--- OS_main.cpp
+++ OS_main.cpp
@@ -5,36 +5,55 @@
 #include<random>
 #include<chrono>
 #include"SemaphoreClass.h"
+#include<unistd.h>
+#include"RGBpixmap.h"
+#ifdef __APPLE__
+#include<GLUT/glut.h>
+#else
+#include<GL/glut.h>
+#endif
 
 using namespace std;
+
 #define THREAD_NUM 6
 #define TABACCO 1
 #define PAPER 3
 #define MATCH 5
-#define MEAN 4
+#define MEAN 4 //for poisson random variable
 #define EMPTY 0
 
-mutex mtx;	//lock which is used to prevent race condition between agents.
-mutex mtx2; //lock which is used to prevent race condition between agents and smoker who can access the table.
-int tableContent = EMPTY;
-Semaphore agSem(0);		/*Semaphore used to wake up one of the Agents to suppy ingredients.*/
-Semaphore table(0);
+mutex mtx;	/*lock which is used to prevent race condition between agents.*/
+mutex mtx2; /*lock which is used to prevent race condition between agents and smoker who can access the table.*/
+Semaphore agSem(0);	/*Semaphore used to wake up one of the Agents to suppy ingredients.*/
+int tableContent;
 int smokerComingTime;
 int smokingTime;
+
+/*
+ * Used to generate poisson random number
+ * */
 default_random_engine generator;
 poisson_distribution<int> distribution(MEAN);
 
-void createThread(thread[]);
+/*
+ * Variables used to set GUI window size & update
+ * */
+int screenWidth = 800;
+int screenHeight = 600;
+int deltaTime = 50000;	//default update time
+
 void cleanTable();
+void createThread(thread[]);
 void smkrTobacco(); //function of smoker with tobacco
 void smkrPaper(); //function of smoker with paper
 void smkrMatch(); //function of smoker with match
-void agTobacco(); //function for supplied of tobacco and paper.
-void agPaper(); //function for supplied of paper and match.
-void agMatch(); //function for supplied of tobacco and match.
+void agTobacco(); //function for supplied of tobacco.
+void agPaper(); //function for supplied of paper.
+void agMatch(); //function for supplied of match.
 
 int main(int argc,char **argv){
 	srand(time(NULL));
+	tableContent = EMPTY;
 	thread myThread[THREAD_NUM];
 	createThread(myThread);
 	agSem.signal();
@@ -59,6 +78,15 @@ void createThread(thread myThread[THREAD_NUM]){
 void cleanTable(){
 	tableContent = 0;
 }
+
+
+/*What do smoker threads do?
+ *(1)wait until there are two resources on the table
+ *(2)acquire lck2 with mtx2 to prevent agent entering its critical section when the smoker is doing its job => race condition
+ *(3)come to table(i.e.,thread sleep) for "smokerComingTime"
+ *(4)decide "smokingTime" and smoke(i.e.,sleep thread again)
+ *(5)clean the table and wake up the agents
+ * */
 
 void smkrTobacco(){
 	while(1){
@@ -111,6 +139,17 @@ void smkrMatch(){
 		}
 	}
 }
+
+
+/*What do agent threads do?
+ *(1)acquire lck with mtx to ensure that no other agents are in critical section
+ *(2)wait for smoker's signal to supply resource.
+ *(3)After signal,acquire lck2 with mtx2 to prevent smoker entering its critical section
+ *   when the agent finished supply ( tableContent += xxx ) but didn't make smokerComingTime. => race condition
+ *(4)if any other agent put something on table, the second agent has to decide "smokerComingTime" before putting its own
+     resource on the table.
+ * */
+
 void agTobacco(){
 	while(1){
 		unique_lock<mutex> lck(mtx);
